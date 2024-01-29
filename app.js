b5468df4372e0a67bac6242debeb0597ae268a7c
Rafael Bustillo <rafabustillo@Rafaels-MacBook-Air.local>
2024-01-29 10:31:00 -0600

    Update Cookie Count

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function displayCookiesGivenLastClass() {
     var cookiesGivenText = document.createElement('p');
 
     // Set the value based on your data or update dynamically
-    var cookiesGivenLastClass = 1;
+    var cookiesGivenLastClass = 3;
 
     cookiesGivenText.textContent = 'Cookies Given Last Class: ' + cookiesGivenLastClass;
     cookiesGivenContainer.appendChild(cookiesGivenText);
